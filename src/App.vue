9579bcb4f9dc65dec1026e17c51c4653e4777680
Robert <robertneagu194@gmail.com>
2020-02-17 00:01:22 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Noto+Sans:400,700&display=swap');
+@import '@/scss/_normalize.scss';
+
+html, body, #app {
+  min-height: 100vh;
+  background-color: black;
+}
+#app {
+  display: flex;
+  flex-direction: column;
+  font-family: 'Noto Sans', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: lighten(teal, 10%);
+  text-shadow: 1px 1px 1px black;
+
+  > * { flex: 1; }
+}
+</style>
