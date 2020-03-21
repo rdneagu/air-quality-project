caefb49b16dff4f8fb7729acdcdf2191d26e6b0b
Robert <robertneagu194@gmail.com>
2020-03-21 23:00:07 +0000

    Fix build serving

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const api = require('./routes/api');
 
@@ -8,6 +9,10 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api', api);
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port);
