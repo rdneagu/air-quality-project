e9b2475b86961efcad0ed847ec67a6c1da9f9023
Robert <robertneagu194@gmail.com>
2020-03-21 23:12:12 +0000

    Fix static file serving

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const api = require('./routes/api');
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../dist')));
 app.use(cors());
 
 app.use('/api', api);
