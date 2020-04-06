27fa5531aa24b52a2c7304fad6a5600152e9eea4
Robert Neagu <robertneagu194@gmail.com>
2020-04-06 22:24:15 +0000

    Add default title to webpages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  chainWebpack(config) {
+    config.plugin('html').tap((args) => {
+      args[0].title = 'Air Quality - World Map'; // eslint-disable-line
+      return args;
+    });
+  },
   css: {
     loaderOptions: {
       scss: {
