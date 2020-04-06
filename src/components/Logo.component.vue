eaa4d3f6e45c909db10559cf9a233d4d10d27d41
Robert Neagu <robertneagu194@gmail.com>
2020-04-06 22:26:09 +0000

    Add logo and account menu

---
+++ src/components/Logo.component.vue
@@ -0,0 +1,70 @@
+<template>
+  <router-link :to="{ name: 'home' }" class="logo">
+    <div class="part-cloud">
+      <Icon name="cloud"></Icon>
+      <span class="text-aqi">CO2</span>
+      <Icon name="arrow_dr"></Icon>
+    </div>
+    <div class="part-name">
+      <span class="text-air">AIR</span>
+      <span class="text-quality">
+        <Icon name="leaf"></Icon>
+        <span>uality</span>
+      </span>
+    </div>
+  </router-link>
+</template>
+
+<script>
+import Icon from './Icon.component.vue';
+
+export default {
+  components: { Icon },
+};
+</script>
+
+<style lang="scss">
+.logo {
+  position: relative;
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+  font-size: 32px;
+  .part-cloud {
+    position: relative;
+    font-size: 1.5em;
+    text-shadow: none;
+    .text-aqi {
+      position: absolute;
+      color: #000;
+      left: 50%;
+      top: calc(50% + 0.35em);
+      transform: translate(-50%, -50%);
+      font-size: 0.25em;
+    }
+    .icon-arrow_dr-aqp {
+      position: absolute;
+      bottom: -0.35em;
+      left: -0.25em;
+      font-size: 1em;
+    }
+  }
+  .part-name {
+    display: flex;
+    margin-left: 0.32em;
+    position: relative;
+    .text-air { margin-right: 0.16em; }
+    .text-quality {
+      display: flex;
+      color: darken($map-fill-color, 2%);
+      font-weight: 600;
+      .icon {
+        display: block;
+        position: relative;
+        transform: rotate(15deg) scaleY(-1);
+        margin-right: -0.16em;
+      }
+    }
+  }
+}
+</style>
--- src/modules/Header.module.vue
+++ src/modules/Header.module.vue
@@ -1,16 +1,61 @@
 <template>
-  <header>
-
+  <header v-if="!isAuthScreen">
+    <Logo></Logo>
+    <div class="navigation"></div>
+    <div class="account">
+      <Button v-bind="account"></Button>
+    </div>
   </header>
 </template>
 
 <script>
-export default {};
+import Logo from '@/components/Logo.component.vue';
+import Button from '@/components/Button.component.vue';
+
+export default {
+  components: { Logo, Button },
+  data() {
+    return {
+      account: {
+        name: 'account',
+        icon: 'arrow_dd',
+        text: 'My Account',
+        type: 'inversed account',
+        dropdown: {
+          id: 'account',
+          ignoreSelection: true,
+          style: 'account',
+          items: {
+            login: { icon: 'person', text: 'Login', type: 'dropdown', href: { name: 'login' } },
+            register: { icon: 'person_add', text: 'Register', type: 'dropdown', href: { name: 'register' } },
+          },
+        },
+      },
+    };
+  },
+  computed: {
+    isAuthScreen() {
+      switch (this.$route.name) {
+        case 'login':
+        case 'register':
+          return true;
+        default:
+          return false;
+      }
+    },
+  },
+};
 </script>
 
 <style lang="scss">
 header {
-  display: flex;
+  grid-area: header;
+  align-self: center;
+  display: grid;
+  grid-template-columns: auto 1fr auto;
+  grid-template-rows: auto;
   align-items: center;
+  padding: 0 20px;
+  // height: 60px;
 }
 </style>
