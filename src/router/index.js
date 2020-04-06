19db03eed0baad1629d906340b0285a78b0905f2
Robert Neagu <robertneagu194@gmail.com>
2020-04-06 22:26:32 +0000

    Add routing for login and register routes

--- src/router/index.js
+++ src/router/index.js
@@ -7,15 +7,39 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home,
+    meta: {
+      title: 'Air Quality - World Map',
+    },
   },
   {
     path: '/about',
-    name: 'About',
+    name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '/login',
+    name: 'login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue'),
+  },
+  {
+    path: '/register',
+    name: 'register',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue'),
+  },
+  {
+    path: '*',
+    name: '404',
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
 ];
