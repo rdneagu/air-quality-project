80156d6bc492df8f9c0a3adc46139d5870e0a69c
Robert Neagu <robertneagu194@gmail.com>
2020-04-08 21:55:14 +0000

    Add vue cookies plugin

--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ import router from './router';
 import store from './store';
 import './directives';
 
+const VueCookies = require('vue-cookies');
+
 Vue.config.productionTip = false;
+
+Vue.use(VueCookies);
+Vue.$cookies.config('1y');
 
 new Vue({
   router,
