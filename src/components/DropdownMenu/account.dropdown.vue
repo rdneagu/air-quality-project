371781fed6a8bbc9455870aeb9c06339ea2acd86
Robert Neagu <robertneagu194@gmail.com>
2020-04-09 14:54:31 +0000

    Create dummy custom account dropdown

---
+++ src/components/DropdownMenu/account.dropdown.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="profile">
+      <div class="avatar"></div>
+      <div class="username"></div>
+    </div>
+    <div class="alerts">
+      <div></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="scss">
+
+</style>
